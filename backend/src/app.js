e1a54c4f02b38f3384cb7df1b2b309170e7e8e1c
Apoorv <yapoorv68@gmail.com>
2026-07-23 11:56:10 +0530

    Configure Express trust proxy for Nginx

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+app.set("trust proxy", 1);
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const dns = require("dns");
